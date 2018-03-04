180a1dc6f0ee9ca5ae8083f2be2b6006aad4d5a2
Keith O'Grady <s00153960@mail.itsligo.ie>
2018-03-04 16:43:33 +0000

    TestProjAdded

---
+++ InsuranceApp/InsuranceApp.test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("InsuranceApp.test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("InsuranceApp.test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c10ac9ca-c496-4f48-b7bb-10767d68002d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ InsuranceApp/InsuranceApp.test/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nunit.Framework;
+
+namespace InsuranceApp.test.Equality 
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public float 12YearOldGirl()
+        {
+            var sut = new Code();
+            var result = sut.calc(12, "female");
+            Assert.That(result, I.EqualTo(6, 0));
+            return result;
+        }
+    }
+}
